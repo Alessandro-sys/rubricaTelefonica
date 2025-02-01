8b6ed77aba04a645e2113f2450ef98e38abd3df4
Alessandro <chiarulli14@gmail.com>
2025-02-01 11:18:50 +0100

    inizio

--- rubricaTelefonica/Program.cs
+++ rubricaTelefonica/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+namespace rubricaTelefonica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
